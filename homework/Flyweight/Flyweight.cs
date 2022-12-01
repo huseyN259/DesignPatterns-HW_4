f65d1a0c12a2a7a6b52cb0bf068ffae75c1cc3f6
user <user@WINDOWS-OM6HICP>
2022-12-01 15:53:20 +0400

    Add project files.

---
+++ homework/Flyweight/Flyweight.cs
@@ -0,0 +1,99 @@
+﻿// Flyweight interface
+interface IGeometry
+{
+	void GetShape();
+	void GetColor(int size);
+}
+
+// ConcreteFlyweight
+class Rectangle : IGeometry
+{
+	public void GetShape()
+		=> Console.WriteLine("This is a Rectangle Shape Object...");
+
+	public void GetColor(int length)
+	{
+		if (length < 5)
+			Console.WriteLine("Rectangle is Filled with PURPLE color...\n");
+		else
+			Console.WriteLine("Rectangle is Filled with WHITE color...\n");
+	}
+}
+
+// ConcreteFlyweight
+class Square : IGeometry
+{
+	public void GetShape()
+		=> Console.WriteLine("This is a Square Shape Object...");
+	
+	public void GetColor(int length)
+	{
+		if (length < 5)
+			Console.WriteLine("Square is Filled with BLACK color...\n");
+		else
+			Console.WriteLine("Square is Filled with GREEN color...\n");
+	}
+}
+
+// FlyweightFactory
+class GeometryFactory
+{
+	Dictionary<string, IGeometry> geometries = new Dictionary<string, IGeometry>();
+
+	public int ObjectCount()
+		=> geometries.Count;
+	
+	public IGeometry getGeometryObject(string name)
+	{
+		IGeometry geometry;
+
+		if (geometries.ContainsKey(name))
+			return geometries[name];
+		else
+		{
+			if (name == "Rectangle")
+			{
+				geometry = new Rectangle();
+				geometries.Add("Rectangle", geometry);
+			}
+			else if (name == "Square")
+			{
+				geometry = new Square();
+				geometries.Add("Square", geometry);
+			}
+			else
+				throw new Exception("This Type of Object Can Not Be Created...");
+		}
+
+		return geometry;
+	}
+}
+
+
+class Program
+{
+	static void Main()
+	{
+		GeometryFactory geometryFactory = new GeometryFactory();
+
+		Console.WriteLine("-------------- Object Details-----------------");
+		IGeometry geometry = geometryFactory.getGeometryObject("Rectangle");
+		geometry.GetShape();
+		geometry.GetColor(9);
+		Console.WriteLine("-------------- Object Details-----------------");
+		geometry = geometryFactory.getGeometryObject("Rectangle");
+		geometry.GetShape();
+		geometry.GetColor(3);
+		Console.WriteLine("-------------- Object Details-----------------");
+		geometry = geometryFactory.getGeometryObject("Square");
+		geometry.GetShape();
+		geometry.GetColor(9);
+		Console.WriteLine("-------------- Object Details-----------------");
+		geometry = geometryFactory.getGeometryObject("Square");
+		geometry.GetShape();
+		geometry.GetColor(3);
+		Console.WriteLine("-------------- Object Count-----------------");
+		int ObjectCount = geometryFactory.ObjectCount();
+		Console.WriteLine("Total objects created: " + ObjectCount);
+	}
+}
---
+++ homework/Proxy/Proxy.cs
@@ -0,0 +1,64 @@
+﻿interface IImage
+{
+	void DisplayImage();
+}
+
+
+class RealImage : IImage
+{
+	private string Filename { get; set; }
+
+	public RealImage(string filename)
+	{
+		Filename = filename;
+		LoadImageFromDisk();
+	}
+
+	public void LoadImageFromDisk()
+		=> Console.WriteLine("Loading Image : " + Filename);
+
+	public void DisplayImage()
+		=> Console.WriteLine("Displaying Image : " + Filename);
+}
+
+
+class ProxyImage : IImage
+{
+	private RealImage? realImage = null;
+
+	private string Filename { get; set; }
+	
+	public ProxyImage(string filename)
+		=> Filename = filename;
+	
+	public void DisplayImage()
+	{
+		if (realImage == null)
+			realImage = new RealImage(Filename);
+		
+		realImage.DisplayImage();
+	}
+}
+
+
+/*class Program
+{
+	static void Main()
+	{
+		IImage Image1 = new ProxyImage("Wolf Image");
+		Console.WriteLine("Image1 calling DisplayImage first time :");
+		Image1.DisplayImage(); // loading necessary
+		Console.WriteLine("Image1 calling DisplayImage second time :");
+		Image1.DisplayImage(); // loading unnecessary
+		Console.WriteLine("Image1 calling DisplayImage third time :");
+		Image1.DisplayImage(); // loading unnecessary
+
+		Console.WriteLine();
+
+		IImage Image2 = new ProxyImage("Horse Image");
+		Console.WriteLine("Image2 calling DisplayImage first time :");
+		Image2.DisplayImage(); // loading necessary
+		Console.WriteLine("Image2 calling DisplayImage second time :");
+		Image2.DisplayImage(); // loading unnecessary
+	}
+}*/
